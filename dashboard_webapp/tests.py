f05d384e8a14a3a7d3ed6ecd9e9e98006f9e55ea
nunezMyles <nunez.myles@gmail.com>
2022-11-30 12:14:12 +0800

    first commit

--- dashboard_webapp/tests.py
+++ dashboard_webapp/tests.py
@@ -1,25 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-async def hello():
-    async with websockets.connect("ws://localhost:8000/ws/pollData", origin="ws://localhost:8000") as websocket:
-        await asyncio.gather(
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-            websocket.send(json.dumps({'value':random.randint(1,100)})),
-        )
-
-
-asyncio.run(hello())
